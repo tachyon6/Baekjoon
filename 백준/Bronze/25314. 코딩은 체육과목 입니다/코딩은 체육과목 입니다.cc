3429a20449de819760f952a06e78c4a7257fea80
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-17 01:36:02 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int num = n/4;
+    for(int i=0; i<num; i++){
+        cout << "long ";
+    }
+    cout << "int";
+    return 0;
+}
