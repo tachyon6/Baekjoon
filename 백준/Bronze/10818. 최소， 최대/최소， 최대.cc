83ab0b2cbfbf5a6472e733b141bd2593b27ef9e4
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-17 02:41:52 +0900

    [Bronze III] Title: 최소, 최대, Time: 180 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -1,25 +1,16 @@
 #include <iostream>
 #include <algorithm>
- 
 using namespace std;
- 
-int main(int argc, const char * argv[]) {
- 
-	ios_base::sync_with_stdio(0);
- 
-	int N;
-	cin >> N;
- 
-	int array[1000001];
- 
-	for(int i = 0; i < N; i++) {
-		cin >> array[i];
-	}
- 
-	sort(array, array + N);		// 0 ~ N-1 범위 정렬
- 
-	cout << array[0] << " " << array[N - 1];
- 
-	return 0;
- 
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    int n, a, k;
+    cin >> n;
+    int arr[1000000];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    cout << arr[0] << " " << arr[n-1];
+    return 0;
 }
