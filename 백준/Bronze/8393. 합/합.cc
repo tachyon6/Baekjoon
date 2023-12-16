aa2e2b829f0724964669b53e5d3d6c764780f23c
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-17 01:29:51 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int sum = 0;
+    cin >> n;
+    for(int i = 0; i<n ; i++){
+        sum += i+1;
+    }
+    cout << sum;
+}
