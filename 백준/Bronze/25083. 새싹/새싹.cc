7f2b517d7519344fe50036bf7685ff6759be9363
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-18 03:10:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cout << "         ,r'\"7" << endl
+         << "r`-_   ,'  ,/" << endl
+         << " \\. \". L_r'" << endl  // 백슬래시와 큰따옴표에 주의
+         << "   `~\\/" << endl
+         << "      |" << endl
+         << "      |";
+    return 0;
+}
