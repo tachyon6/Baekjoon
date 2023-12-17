1c87a29c19af46c8aac95a8dec5a30c159ec0c58
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-17 22:44:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    char s;
+    cin >> s;
+    int i = int(s);
+    cout << i ;
+    return 0;
+}
