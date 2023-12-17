10ce564a15113e4a692d56cd8a4472ad7e8af0dd
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-18 00:38:49 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+
+    int t, r;
+    string s;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        cin >> r >> s;
+        for(int j = 0; j < s.length(); j++){
+            for(int k = 0; k < r; k++){
+                cout << s[j];
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
