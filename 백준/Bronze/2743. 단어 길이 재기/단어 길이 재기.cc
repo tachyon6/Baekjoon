36f5a8b8d7a2ef59a01897568709ee46763127f3
tachyon6 <127651058+tachyon6@users.noreply.github.com>
2023-12-17 22:37:38 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    string s;
+    cin >> s;
+    cout << s.length();
+    return 0;
+}
